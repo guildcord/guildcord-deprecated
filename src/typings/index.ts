2492d787baf27092ba49772892eba8ec45b6a324
Colderry <colderryz@gmail.com>
2021-07-28 22:42:51 +0600

    More typings

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,1 +1,24 @@
 export type EmbedColors = "Blue" | "Green" | "Grey" | "Red" | "Pain";
+export type RequestMethods = "GET" | "POST" | "PUT" | "DELETE";
+
+export const ClientEvents = {
+    Hello: "ready"
+};
+
+export interface APIClientUser {
+    id: string;
+    name: string;
+    subdomain: string | null;
+    aliases: string[] | [];
+    profilePictureSm: string;
+    profilePicture: string;
+    profilePictureLg: string;
+    profilePictureBlur: string;
+    profileBannerBlur: string;
+    profileBannerLg: string;
+    joinDate: string;
+    steamId: string;
+    moderationStatus: string;
+    aboutInfo: string;
+    lastOnline: string;
+};
